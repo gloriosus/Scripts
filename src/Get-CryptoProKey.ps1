204813f77f4f49fa655d3f1b85ee37f78ed24b30
Vladislav Bondarenko <vladislav@bondarenko.page>
2022-04-05 22:25:58 +1000

    feat: add a script for getting product keys of the CryptoPro CSP installed on a machine

---
+++ src/Get-CryptoProKey.ps1
@@ -0,0 +1,32 @@
+Write-Host "How to output a result? Type 'file' or 'console'"
+$outputMethod = Read-Host
+Write-Host "`n"
+
+$products = Get-ChildItem -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Installer\UserData\S-1-5-18\Products"
+
+foreach ($product in $products)
+{
+    $propertiesPath = "Registry::$($product)\InstallProperties"
+    $isExists = Test-Path -Path $propertiesPath
+
+    if ($isExists)
+    {
+        $properties = Get-ItemProperty -Path $propertiesPath
+
+        if ($properties.DisplayName -eq "КриптоПро CSP")
+        {
+            $result = "$($properties.DisplayName) v.$($properties.Version): $($properties.ProductId)"
+
+            switch ($outputMethod) 
+            {
+                "file" { $result | Out-File -FilePath "$($PWD)\CryptoPro $($properties.Version) ProductId.txt" }
+                "console" { Write-Host $result }
+                default { Write-Host $result }
+            }
+        }
+    }
+}
+
+Write-Host "`n"
+Write-Host "The task has been finished. Press Enter to exit"
+Read-Host
