d7440c77ad208436290b4ffaf1a7311367e0460e
Vladislav Bondarenko <vladislav@bondarenko.page>
2023-02-07 17:52:44 +1000

    Make manual uninstallation of Google Chrome

--- Source/Uninstall-GoogleChrome.ps1
+++ Source/Uninstall-GoogleChrome.ps1
@@ -1,3 +1,7 @@
+$RootFolder = (Split-Path -Path $PSScriptRoot -Parent)
+
+Import-Module -Name "$($RootFolder)\Modules\Jarnet.PowerShell.Utility"
+
 $SystemType = (Get-CimInstance -ClassName Win32_ComputerSystem).SystemType
 $SystemType -Match "x(?<arch>64|86)-based PC"
 $Arch = $Matches.arch
@@ -9,6 +13,21 @@ if ($Arch -eq "86") {
 }
 
 $GoogleChromePath = (Get-ChildItem -Path $RegistryPath | Get-ItemProperty | Where-Object DisplayName -like "*Google Chrome*").PSPath
-$UninstallString = Get-ItemProperty -Path $GoogleChromePath -Name "UninstallString"
-$UninstallString -match "`"(?<file>.*)`"(.?)(?<args>.*)"
-Start-Process -FilePath $Matches.file -ArgumentList $Matches.args
+Remove-ScService -Name "GoogleChromeElevationService" -Timeout 30
+
+Stop-Process -Name "setup", "chrome" -Force -ErrorAction SilentlyContinue | Wait-Process -Timeout 30
+
+$UserName = (Get-CimInstance -Class Win32_ComputerSystem).UserName.Split('\')[1]
+$Paths = @(
+    "C:\Program Files\Google\Chrome",
+    "C:\Program Files\Google\Update",
+    "C:\Program Files (x86)\Google\Update",
+    "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Google Chrome.lnk",
+    "C:\Users\$($UserName)\AppData\Local\Google",
+    "C:\Users\$($UserName)\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\Google Chrome.lnk",
+    "C:\Users\$($UserName)\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Google Chrome.lnk",
+    "C:\Users\Public\Desktop\Google Chrome.lnk",
+    $GoogleChromePath
+)
+
+Remove-Item -Path $Paths -Recurse -Force -ErrorAction SilentlyContinue
