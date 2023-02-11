9c88e60b6f0e87cbf7ffc830f45ce59518e09336
Vladislav Bondarenko <vladislav@bondarenko.page>
2023-02-11 14:00:54 +1000

    Split-Mp3: check for the presense of the playlist.csv and playlist.mp3 files in the specified location

--- Source/Split-Mp3.ps1
+++ Source/Split-Mp3.ps1
@@ -8,6 +8,17 @@ while (-not (Test-Path -Path $Location)) {
     $Location = Read-Host "Please, specify the correct location of a playlist"
 }
 
+$CsvExists = Test-Path -Path (Join-Path -Path $Location -ChildPath "playlist.csv")
+$Mp3Exists = Test-Path -Path (Join-Path -Path $Location -ChildPath "playlist.mp3")
+
+while ((-not $CsvExists) -or (-not $Mp3Exists)) {
+    Write-Host "Please, make sure that the playlist.csv and playlist.mp3 files are present in the specified location and press Enter"
+    if ([Console]::ReadKey().Key -eq "Enter") {
+        $CsvExists = Test-Path -Path (Join-Path -Path $Location -ChildPath "playlist.csv")
+        $Mp3Exists = Test-Path -Path (Join-Path -Path $Location -ChildPath "playlist.mp3")
+    }
+}
+
 Set-Location -Path $Location
 
 $Playlist = Get-Content ".\playlist.csv" | ConvertFrom-Csv -Delimiter ";"
@@ -18,7 +29,7 @@ foreach ($Track in $Playlist) {
     $TrackNumber = "{0:d2}" -f [int]$Track.Track
     $Duration = ([TimeSpan]$Track.Duration).TotalSeconds
 
-    Invoke-Command { ffmpeg -ss $Seek -i ".\playlist.mp3" -t $Duration -c:a libmp3lame -b:a 320k -metadata track="$($Track.Track)" -metadata artist="$($Track.Artist)" -metadata title="$($Track.Title)" -metadata album="$($Track.Album)" -metadata date="$($Track.Year)" ".\$TrackNumber. $($Track.Artist) - $($Track.Title) - $($Track.Album).mp3" }
+    Invoke-Command { ffmpeg -ss $Seek -y -i ".\playlist.mp3" -t $Duration -c:a libmp3lame -b:a 320k -metadata track="$($Track.Track)" -metadata artist="$($Track.Artist)" -metadata title="$($Track.Title)" -metadata album="$($Track.Album)" -metadata date="$($Track.Year)" ".\$TrackNumber. $($Track.Artist) - $($Track.Title) - $($Track.Album).mp3" }
 
     $Seek = $Duration
 }
