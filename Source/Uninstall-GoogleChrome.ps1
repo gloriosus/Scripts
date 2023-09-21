0a64797d72689ad16460047fb89a7a09961a8c0b
Vladislav Bondarenko <vladislav@bondarenko.page>
2023-09-21 21:13:52 +1000

    Extract the uninstalling string and invoke it to uninstall Google Chrome

--- Source/Uninstall-GoogleChrome.ps1
+++ Source/Uninstall-GoogleChrome.ps1
@@ -13,21 +13,21 @@ if ($Arch -eq "86") {
 }
 
 $GoogleChromePath = (Get-ChildItem -Path $RegistryPath | Get-ItemProperty | Where-Object DisplayName -like "*Google Chrome*").PSPath
-Remove-ScService -Name "GoogleChromeElevationService" -Timeout 30
+$UninstallString = (Get-ItemProperty -Path $GoogleChromePath).UninstallString
+$UninstallString -Match "(?<base>.*)--verbose-logging"
+$UninstallString = $Matches.base + "--force-uninstall"
 
+Remove-ScService -Name "GoogleChromeElevationService" -Timeout 30
 Stop-Process -Name "setup", "chrome" -Force -ErrorAction SilentlyContinue | Wait-Process -Timeout 30
+Invoke-Expression -Command "& $($UninstallString)"
 
 $UserName = (Get-CimInstance -Class Win32_ComputerSystem).UserName.Split('\')[1]
 $Paths = @(
-    "C:\Program Files\Google\Chrome",
-    "C:\Program Files\Google\Update",
-    "C:\Program Files (x86)\Google\Update",
     "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Google Chrome.lnk",
     "C:\Users\$($UserName)\AppData\Local\Google",
     "C:\Users\$($UserName)\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\Google Chrome.lnk",
     "C:\Users\$($UserName)\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Google Chrome.lnk",
-    "C:\Users\Public\Desktop\Google Chrome.lnk",
-    $GoogleChromePath
+    "C:\Users\Public\Desktop\Google Chrome.lnk"
 )
 
 Remove-Item -Path $Paths -Recurse -Force -ErrorAction SilentlyContinue
